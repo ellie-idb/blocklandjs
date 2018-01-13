21764843d50c0a283a5284ab8c42fc47ed61c65b
hafo <harrison@malw.io>
2018-01-12 22:39:58 -0700

    Update Torque.cpp

--- Torque.cpp
+++ Torque.cpp
@@ -1,5 +1,8 @@
 #include "torque.h"
 #include <psapi.h>
+#include <varargs.h>
+#include <cstdio>
+#include <cstdarg>
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -66,6 +69,146 @@ GetGlobalVariableFn GetGlobalVariable;
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Functions
+
+static std::map<Identifier*, Namespace::Entry*> cache;
+static std::map<const char*, Namespace*> nscache;
+static Namespace* GlobalNS;
+
+void rewrite__fatal() {
+	Printf("!!! THIS SHOULD NEVER HAPPEN !!!");
+}
+
+Namespace::Entry* fastLookup(const char* ourNamespace, const char* name) {
+	Namespace* ns;
+	Namespace::Entry* entry;
+
+	if (_stricmp(ourNamespace, "") == 0) { //If the namespace is blank, assume we're looking in the global namespace.
+		if (GlobalNS == NULL) {
+			GlobalNS = LookupNamespace(NULL);
+		}
+		ns = GlobalNS;
+	}
+	else {
+		std::map<const char*, Namespace*>::iterator its;
+		its = nscache.find(ourNamespace);
+		if (its != nscache.end()) {
+			ns = its->second;
+			if (ns == NULL) {
+				//Somehow it got nullptr'd..
+				ns = LookupNamespace(ourNamespace);
+				if (ns == NULL) {
+					//THIS SHOULD NEVER HAPPEN!
+					rewrite__fatal();
+					Printf("Fatal: Found cached NS entry with nullptr, could not find namespace!");
+					nscache.erase(its);
+					return nullptr;
+				}
+				nscache.erase(its);
+				nscache.insert(nscache.end(), std::make_pair(ourNamespace, ns));
+			}
+		}
+		else {
+			ns = LookupNamespace(StringTableEntry(ourNamespace));
+			if (ns == NULL) {
+				rewrite__fatal();
+				Printf("Fatal: fastLookup FAILED (2/2)!");
+				return nullptr;
+			}
+			nscache.insert(nscache.end(), std::make_pair(ourNamespace, ns));
+		}
+	}
+
+	std::map<Identifier*, Namespace::Entry*>::iterator it;
+	Identifier* lol = new Identifier();
+	lol->mName = name;
+	lol->mNamespace = ourNamespace;
+	it = cache.find(lol); //Look into our Namespace::Entry* cache..
+	if (it != cache.end()) {
+		entry = it->second;
+		if (entry == NULL) {
+			rewrite__fatal();
+			Printf("Fatal: found nullptr in cache!");
+			cache.erase(it); //Erase it so we don't encounter it again.
+			delete lol;
+			return nullptr;
+		}
+	}
+	else {
+		entry = Namespace__lookup(ns, StringTableEntry(name));
+		if (entry == NULL) {
+			Printf("Could not find function.");
+			delete lol;
+			return nullptr;
+		}
+		cache.insert(cache.end(), std::make_pair(lol, entry)); //Insert it so further calls are optimized.
+	}
+
+	return entry;
+}
+
+void* ts__fastCall(Namespace::Entry* ourCall, SimObject* obj, unsigned argc, ...) {
+	if (ourCall == NULL) {
+		Printf("Invalid entry.");
+		return nullptr;
+	}
+	if (argc > 19) {
+		Printf("Too many arguments for torquescript to handle..");
+		return nullptr;
+	}
+	const char* argv[21];
+	va_list args;
+	va_start(args, argc);
+	argv[0] = ourCall->mFunctionName;
+	for (int i = 0; i < argc; i++) {
+		argv[i + 1] = va_arg(args, const char*);
+	}
+	argc++;
+	va_end(args);
+	if (ourCall->mType == Namespace::Entry::ScriptFunctionType) {
+		if (ourCall->mFunctionOffset) {
+			const char* retVal = CodeBlock__exec(
+				ourCall->mCode, ourCall->mFunctionOffset,
+				ourCall->mNamespace, ourCall->mFunctionName,
+				argc, argv, false, ourCall->mNamespace->mPackage,
+				0);
+			return (void*)retVal; //we know what it's supposed to return.
+		}
+		else {
+			return nullptr;
+		}
+	}
+	S32 mMinArgs = ourCall->mMinArgs, mMaxArgs = ourCall->mMaxArgs;
+	if ((mMinArgs && argc < mMinArgs) || (mMaxArgs && argc > mMaxArgs)) {
+		Printf("Expected args between %d and %d, got %d", mMinArgs, mMaxArgs, argc);
+		return nullptr;
+	}
+	SimObject* actualObj;
+	if (obj == NULL) {
+		actualObj = NULL;
+	}
+	else
+	{
+		actualObj = obj;
+	}
+	switch (ourCall->mType) {
+	case Namespace::Entry::StringCallbackType:
+		return (void*)ourCall->cb.mStringCallbackFunc(actualObj, argc, argv);
+	case Namespace::Entry::IntCallbackType:
+		return (void*)ourCall->cb.mIntCallbackFunc(actualObj, argc, argv);
+	case Namespace::Entry::FloatCallbackType: {
+		//Wtf?
+		float ourret[] = { ourCall->cb.mFloatCallbackFunc(actualObj, argc, argv) };
+		return (void*)ourret;
+	}
+	case Namespace::Entry::BoolCallbackType:
+		return (void*)ourCall->cb.mBoolCallbackFunc(actualObj, argc, argv);
+	case Namespace::Entry::VoidCallbackType:
+		ourCall->cb.mVoidCallbackFunc(actualObj, argc, argv);
+		return nullptr;
+	}
+
+	return nullptr;
+}
 
 //Set the module start and length
 void InitScanner()
