6761dfaa668bee76c2cca5d8e29ca1d1baa0250d
hatf0 <harrison@malw.io>
2018-01-25 11:48:15 -0700

    Added TCP callbacks in school cuz anxiety a fuck

--- BaseBlocklandDLL/tcp.cpp
+++ BaseBlocklandDLL/tcp.cpp
@@ -38,6 +38,25 @@ uv8_efunc(uv8_tcp_connect) {
 	uv8_unfinished_args();
 }
 
+uv8_efunc(uv8_tcp_onread) {
+	
+}
+
+uv8_efunc(uv8_tcp_onwrite) {
+	
+}
+
+uv8_efunc(uv8_tcp_onconnect) {
+	
+}
+
+uv8_efunc(uv8_tcp_onshutdown) {
+	
+}
+
+uv8_efunc(uv8_tcp_onconnection) {
+	
+}
 /*
 Handle<ObjectTemplate> uv8_bind_tcp(Isolate* this_) {
 	Handle<ObjectTemplate> tcp = ObjectTemplate::New(this_);
@@ -54,6 +73,11 @@ void uv8_init_tcp(Isolate* this_) {
 	tcp->Set(this_, "getpeername", FunctionTemplate::New(this_, uv8_tcp_getpeername));
 	tcp->Set(this_, "getsockname", FunctionTemplate::New(this_, uv8_tcp_getsockname));
 	tcp->Set(this_, "connect", FunctionTemplate::New(this_, uv8_tcp_connect));
+	tcp->Set(this_, "onRead", FunctionTemplate::New(this_, uv8_tcp_onread));
+	tcp->Set(this_, "onWrite", FunctionTemplate::New(this_, uv8_tcp_onwrite));
+	tcp->Set(this_, "onConnect", FunctionTemplate::New(this_, uv8_tcp_onconnect));
+	tcp->Set(this_, "onShutdown", FunctionTemplate::New(this_, uv8_tcp_onshutdown));
+	tcp->Set(this_, "onConnected", FunctionTemplate::New(this_, uv8_tcp_onconnected));
 	tcp->SetInternalFieldCount(1);
 
 	uvtcp.Reset(this_, tcp);
